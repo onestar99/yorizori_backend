e673ac4b1377e4289d1bac660764f3e05a60ad7d
onestar99 <jhb990438@gmail.com>
2023-07-10 20:35:36 +0900

    Controller, Repository, Service, DTO, Entity 패키지 분리

---
+++ src/main/java/com/kkkj/yorijori_be/Entity/User/UserEntity.java
@@ -0,0 +1,36 @@
+package com.kkkj.yorijori_be.Entity.User;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@ToString
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "User")
+@Entity
+public class UserEntity {
+
+    @Id
+    private String UserTokenId;
+
+    @Column
+    private String NickName;
+
+    @Column
+    private String ImageAddress;
+
+    @Column
+    private String Age;
+
+    @Column
+    private String Gender;
+
+    @Column
+    private String OAuthDivision;
+
+
+
+}
