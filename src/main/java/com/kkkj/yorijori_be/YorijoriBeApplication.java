89ec8b44219347a92910bd764371732abc8b7d0a
onestar99 <jhb990438@gmail.com>
2023-07-10 11:07:51 +0900

    Initial commit

---
+++ src/main/java/com/kkkj/yorijori_be/YorijoriBeApplication.java
@@ -0,0 +1,13 @@
+package com.kkkj.yorijori_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YorijoriBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YorijoriBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kkkj/yorijori_be/YorijoriBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkkj.yorijori_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YorijoriBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
