af47e4d9d2eabcb40384e4145322a9e093479296
onestar99 <jhb990438@gmail.com>
2023-07-10 21:41:22 +0900

    Test 교육용

--- src/main/java/com/kkkj/yorijori_be/Repository/User/UserRepository.java
+++ src/main/java/com/kkkj/yorijori_be/Repository/User/UserRepository.java
@@ -3,7 +3,5 @@ package com.kkkj.yorijori_be.Repository.User;
 import com.kkkj.yorijori_be.Entity.User.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserEntity,String> {
-
-
+public interface UserRepository extends JpaRepository<UserEntity, String> {
 }
