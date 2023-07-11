e2691e19fc597020832826143f25dbb38bdcb33c
HanByeol <jhb990438@gmail.com>
2023-07-11 16:08:59 +0900

    토큰Id를 입력받아 닉네임 업데이트 기능 추가

--- src/main/java/com/kkkj/yorijori_be/Controller/User/UserUpdateController.java
+++ src/main/java/com/kkkj/yorijori_be/Controller/User/UserUpdateController.java
@@ -3,10 +3,7 @@ package com.kkkj.yorijori_be.Controller.User;
 import com.kkkj.yorijori_be.Service.User.UserSaveUpdateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,11 +16,13 @@ public class UserUpdateController {
 
     // 토큰 아이디를 통한 닉네임 수정
     // return 변경된 닉네임
-    @GetMapping("/{tokenId}") @ResponseBody
-    public String updateNickName(@PathVariable String tokenId){
-
-        userSaveUpdateService.updateNickNameById(tokenId, "할게많네요");
-        return "할게많네요";
+    @GetMapping("") @ResponseBody
+    public String updateNickName(
+            @RequestParam(value = "tokenId", required = true) String tokenId,
+            @RequestParam(value = "nickName", required = true) String nickName
+    ){
+        userSaveUpdateService.updateNickNameById(tokenId, nickName);
+        return nickName;
     }
 
 }
