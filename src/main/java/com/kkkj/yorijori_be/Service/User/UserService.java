285fef6832edeb1c1b037258057b4d1118c1e6bf
HanByeol <jhb990438@gmail.com>
2023-07-11 15:00:32 +0900

    유저 정보 페이징 처리 추가 및 컨트롤러 Get, Save 분리

--- src/main/java/com/kkkj/yorijori_be/Service/User/UserService.java
+++ src/main/java/com/kkkj/yorijori_be/Service/User/UserService.java
@@ -6,7 +6,6 @@ import com.kkkj.yorijori_be.Repository.User.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -44,14 +43,44 @@ public class UserService {
         return userDtoList;
     }
 
+
+    // 유저 정보 페이징해서 보내기.
+    public Page<UserEntity> getUserPaging(int pageNo, int pageSize, String sortBy){
+
+        // 페이지 인스턴스 생성
+//        Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by(sortBy).descending());
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        return userRepository.findAll(pageable);
+
+    }
+//
+//    // 유저 정보 페이징해서 보내기.
+//    public List<UserDto> getUserPaging(int pageNo, int pageSize, String sortBy){
+//
+//        // 페이지 인스턴스 생성
+////        Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by(sortBy).descending());
+//        Pageable pageable = PageRequest.of(pageNo, pageSize);
+//        Page<UserEntity> userPage = userRepository.findAll(pageable);
+//
+//        List<UserDto> userDtoList = new ArrayList<>();
+//
+//        for (UserEntity userEntity: userPage){
+//            userDtoList.add(UserDto.toUserDto(userEntity));
+//        }
+//        return userDtoList;
+//
+//    }
+
+
+
+
     // 개인 유저 검색
     public UserDto findUserByTokenId(String userTokenId){
 
         Optional<UserEntity> optionalUserEntity = userRepository.findById(userTokenId);
         if(optionalUserEntity.isPresent()) {
             UserEntity userEntity = optionalUserEntity.get();
-            UserDto userDto = UserDto.toUserDto(userEntity);
-            return userDto;
+            return UserDto.toUserDto(userEntity);
         } else {
             return null;
         }
