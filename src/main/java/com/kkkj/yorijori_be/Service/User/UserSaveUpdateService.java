b806ca551e7df42745cc2a928a089d4653701744
HanByeol <jhb990438@gmail.com>
2023-07-12 11:23:37 +0900

    UserComment Save 추가

--- src/main/java/com/kkkj/yorijori_be/Service/User/UserSaveUpdateService.java
+++ src/main/java/com/kkkj/yorijori_be/Service/User/UserSaveUpdateService.java
@@ -1,6 +1,8 @@
 package com.kkkj.yorijori_be.Service.User;
 
+import com.kkkj.yorijori_be.Dto.User.UserCommentDto;
 import com.kkkj.yorijori_be.Dto.User.UserDto;
+import com.kkkj.yorijori_be.Entity.User.UserCommentEntity;
 import com.kkkj.yorijori_be.Entity.User.UserEntity;
 import com.kkkj.yorijori_be.Repository.User.UserRepository;
 import jakarta.transaction.Transactional;
@@ -26,8 +28,25 @@ public class UserSaveUpdateService {
     @Transactional
     public void updateNickNameById(String tokenId, String NickName){
         UserDto userDto = UserDto.toUserDto(userRepository.findById(tokenId).get());
-        userDto.setNickName(NickName);
+        userDto.setNickname(NickName);
         userRepository.save(userDto.toEntity());
+    }
+
+
+    @Transactional
+    public void saveUserComment(String userTokenId, UserCommentDto userCommentDto){
+
+        // TokenId를 통해 유저 정보 찾기
+        UserEntity userEntity = userRepository.findByUserTokenId(userTokenId);
+        // 전달받은 DTO를 Entity로 변경
+        UserCommentEntity userCommentEntity = userCommentDto.toEntity();
+        // Comment Entity 유저 정보 세팅
+        userCommentEntity.setUser(userEntity);
+        // User에 Comment Entity 추가
+        userEntity.getComments().add(userCommentEntity);
+        // 저장
+        userRepository.save(userEntity);
+
     }
 
 
