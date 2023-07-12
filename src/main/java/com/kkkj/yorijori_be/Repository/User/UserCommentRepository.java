07915bee465212bd7eb93e4a16511002e569e9d4
HanByeol <jhb990438@gmail.com>
2023-07-12 11:23:50 +0900

    UserComment Repository 추가

---
+++ src/main/java/com/kkkj/yorijori_be/Repository/User/UserCommentRepository.java
@@ -0,0 +1,9 @@
+package com.kkkj.yorijori_be.Repository.User;
+
+import com.kkkj.yorijori_be.Entity.User.UserCommentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserCommentRepository extends JpaRepository<UserCommentEntity, Long> {
+
+
+}
