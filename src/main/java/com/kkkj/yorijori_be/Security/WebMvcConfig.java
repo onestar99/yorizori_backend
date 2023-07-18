3d9bfacf3d0c9846cea338c3b6e16396ea7397d8
HanByeol <jhb990438@gmail.com>
2023-07-18 10:37:26 +0900

    React 요청 CORS 허용

---
+++ src/main/java/com/kkkj/yorijori_be/Security/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.kkkj.yorijori_be.Security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+
+    /*
+    *
+    * React Origin 요청 -> cors 허용
+    *
+    * */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("OPTIONS","GET","POST","PUT","DELETE");
+    }
+
+}
