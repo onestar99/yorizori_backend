d901cd8716e69cb846a1694414ee46f1820347d0
HanByeol <jhb990438@gmail.com>
2023-07-19 16:27:22 +0900

    업로드 기능 List로 변경

--- src/main/java/com/kkkj/yorijori_be/Cloud/S3UploadController.java
+++ src/main/java/com/kkkj/yorijori_be/Cloud/S3UploadController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,10 +24,11 @@ public class S3UploadController {
 
     //유저 프로필 업로드
     @PostMapping("user/save/profilePhoto")
-    public ResponseEntity uploadProfilePhoto(@RequestParam("profilePhoto") MultipartFile multipartFile) throws IOException {
+    public ResponseEntity uploadProfilePhoto(@RequestParam("profilePhoto") List<MultipartFile> multipartFileList) throws IOException {
 
         //S3 Bucket 내부에 "/profile" 폴더
-        FileUploadResponse fileUploadResponse = s3Uploader.upload("123", multipartFile, "profile");
-        return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.LOGIN_SUCCESS, fileUploadResponse), HttpStatus.OK);
+        List<FileUploadResponse> fileUploadResponseList = s3Uploader.upload("123", multipartFileList, "profile");
+
+        return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.UPLOAD_SUCCESS, fileUploadResponseList), HttpStatus.OK);
     }
 }
