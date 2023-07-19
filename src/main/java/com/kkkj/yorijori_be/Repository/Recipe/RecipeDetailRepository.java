52b92e40647fafba5c3b234f633d3e40ea001b62
HanByeol <jhb990438@gmail.com>
2023-07-19 16:26:57 +0900

    Recipe Detail Entity 수정

--- src/main/java/com/kkkj/yorijori_be/Repository/Recipe/RecipeDetailRepository.java
+++ src/main/java/com/kkkj/yorijori_be/Repository/Recipe/RecipeDetailRepository.java
@@ -3,5 +3,5 @@ package com.kkkj.yorijori_be.Repository.Recipe;
 import com.kkkj.yorijori_be.Entity.Recipe.RecipeDetailEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RecipeDetailRepository extends JpaRepository<RecipeDetailEntity, Integer> {
+public interface RecipeDetailRepository extends JpaRepository<RecipeDetailEntity, Long> {
 }
