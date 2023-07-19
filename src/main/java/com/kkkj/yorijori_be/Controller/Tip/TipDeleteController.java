8791be482987f828afebfdab372b0689bc0079ce
johndudwns <92994411+johndudwns@users.noreply.github.com>
2023-07-19 14:39:32 +0900

    TipDelete 완성

--- src/main/java/com/kkkj/yorijori_be/Controller/Tip/TipDeleteController.java
+++ src/main/java/com/kkkj/yorijori_be/Controller/Tip/TipDeleteController.java
@@ -1,11 +1,25 @@
 package com.kkkj.yorijori_be.Controller.Tip;
 
+import com.kkkj.yorijori_be.Service.Tip.TipDeleteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/tip/delete")
 public class TipDeleteController {
+    private final TipDeleteService tipDeleteService;
+
+    @GetMapping("")
+    @ResponseBody
+    public String deleteTip(
+            @RequestParam(value = "tokenId", required = true) String tokenId
+    ){
+        tipDeleteService.deleteTipById(tokenId);
+        return "deleted";
+    }
 }
--- src/main/java/com/kkkj/yorijori_be/Service/Tip/TipDeleteService.java
+++ src/main/java/com/kkkj/yorijori_be/Service/Tip/TipDeleteService.java
@@ -1,4 +1,21 @@
 package com.kkkj.yorijori_be.Service.Tip;
 
+import com.kkkj.yorijori_be.Dto.Tip.TipDto;
+import com.kkkj.yorijori_be.Repository.Tip.TipRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class TipDeleteService {
+    private final TipRepository tipRepository;
+
+    @Transactional
+    public void deleteTipById(String tokenId){
+        TipDto tipDto = TipDto.toTipDto(tipRepository.findById(tokenId).get());
+        tipRepository.delete(tipDto.toEntity());
+    }
 }
