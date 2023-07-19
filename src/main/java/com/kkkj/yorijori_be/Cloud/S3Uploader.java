ac05f414f760ba79331ba2b62e2211a764d63734
HanByeol <jhb990438@gmail.com>
2023-07-19 16:27:22 +0900

    업로드 기능 List로 변경

--- src/main/java/com/kkkj/yorijori_be/Cloud/S3Uploader.java
+++ src/main/java/com/kkkj/yorijori_be/Cloud/S3Uploader.java
@@ -15,9 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -30,12 +28,18 @@ public class S3Uploader {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    public FileUploadResponse upload(String userId, MultipartFile multipartFile, String dirName) throws IOException {
+    public List<FileUploadResponse> upload(String userId, List<MultipartFile> multipartFileList, String dirName) throws IOException {
 
-        File uploadFile = convert(multipartFile)
-                .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File로 전환이 실패했습니다."));
 
-        return upload(userId, uploadFile, dirName);
+        List<FileUploadResponse> fileUploadResponseList = new ArrayList<>();
+
+        for(MultipartFile multipartFile: multipartFileList){
+            File uploadFile = convert(multipartFile)
+                    .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File로 전환이 실패했습니다."));
+            fileUploadResponseList.add(upload(userId, uploadFile, dirName));
+        }
+
+        return fileUploadResponseList;
     }
 
 
@@ -44,8 +48,8 @@ public class S3Uploader {
         log.info("파일 확장자 : " + getExtension(uploadFile));
         String Extension = getExtension(uploadFile);
         /*
-        * TODO 여기에 PNG, JPEG 사진 파일만을 거르도록 해주는 함수 추가하기.
-        *
+        * TODO PNG, JPEG 사진 파일만을 거르도록 해주는 함수 추가하기.
+        * -완료-
         * */
 
         if (Objects.equals(Extension, "png") || Objects.equals(Extension, "jpeg")){
@@ -63,6 +67,7 @@ public class S3Uploader {
             return new FileUploadResponse(fileName, uploadImageUrl);
             //return uploadImageUrl;
         } else {
+            removeNewFile(uploadFile);
             return new FileUploadResponse("null", "null");
         }
 
@@ -76,6 +81,7 @@ public class S3Uploader {
         return amazonS3Client.getUrl(bucket, fileName).toString();
     }
 
+    // 로컬에서 만들어진 파일 삭제
     private void removeNewFile(File targetFile) {
         if (targetFile.delete()) {
             log.info("파일이 로컬에서 삭제되었습니다.");
@@ -103,6 +109,8 @@ public class S3Uploader {
 
 
 
+    // 파일의 확장자 명칭을 return.
+    // Ex) png, jpeg, exe
     public String getExtension(File File) {
         String fileName = File.getName();
         log.info(fileName);
