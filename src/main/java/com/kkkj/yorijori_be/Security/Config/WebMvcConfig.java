191b44334629a25a3faa689f7460cbe335835d26
HanByeol <jhb990438@gmail.com>
2023-07-26 16:41:03 +0900

    빌드 정리

--- src/main/java/com/kkkj/yorijori_be/Security/Config/WebMvcConfig.java
+++ src/main/java/com/kkkj/yorijori_be/Security/Config/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package com.kkkj.yorijori_be.Security;
+package com.kkkj.yorijori_be.Security.Config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
--- src/main/java/com/kkkj/yorijori_be/Security/Login/LoginController.java
+++
@@ -1,8 +0,0 @@
-package com.kkkj.yorijori_be.Security.Login;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class LoginController {
-
-}
