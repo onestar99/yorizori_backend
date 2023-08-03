04f6f8f6650548572026e588646c09ef506d531e
HanByeol <jhb990438@gmail.com>
2023-08-03 16:53:45 +0900

    Spring Security gradle, Configuration 추가

---
+++ src/main/java/com/kkkj/yorijori_be/Security/Config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.kkkj.yorijori_be.Security.Config;
+
+import com.kkkj.yorijori_be.Repository.User.UserRepository;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+
+    private final UserRepository userRepository;
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http
+                .authorizeHttpRequests()
+                .requestMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+
+        return http.build();
+    }
+
+
+
+
+
+}
