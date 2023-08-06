e4028665d92677a5e33f16aa94eddca4d46491b9
onestar99 <jhb990438@gmail.com>
2023-08-06 21:29:21 +0900

    자바8 버전 이슈 해결

--- src/main/java/com/kkkj/yorijori_be/Repository/User/UserCommentRepository.java
+++ src/main/java/com/kkkj/yorijori_be/Repository/User/UserCommentRepository.java
@@ -4,6 +4,7 @@ import com.kkkj.yorijori_be.Entity.Recipe.RecipeEntity;
 import com.kkkj.yorijori_be.Entity.User.UserCommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -16,15 +17,19 @@ public interface UserCommentRepository extends JpaRepository<UserCommentEntity, 
     * 게시글 댓글 별점별 개수
     * */
     @Query("SELECT COUNT(c) FROM UserCommentEntity c WHERE c.board.recipeId = :boardId AND c.scope = '5.0'")
-    Long countCommentsWithScope5ForBoard(Long boardId);
+    Long countCommentsWithScope5ForBoard(@Param("boardId")Long boardId);
+
     @Query("SELECT COUNT(c) FROM UserCommentEntity c WHERE c.board.recipeId = :boardId AND c.scope = '4.0'")
-    Long countCommentsWithScope4ForBoard(Long boardId);
+    Long countCommentsWithScope4ForBoard(@Param("boardId")Long boardId);
+
     @Query("SELECT COUNT(c) FROM UserCommentEntity c WHERE c.board.recipeId = :boardId AND c.scope = '3.0'")
-    Long countCommentsWithScope3ForBoard(Long boardId);
+    Long countCommentsWithScope3ForBoard(@Param("boardId")Long boardId);
+
     @Query("SELECT COUNT(c) FROM UserCommentEntity c WHERE c.board.recipeId = :boardId AND c.scope = '2.0'")
-    Long countCommentsWithScope2ForBoard(Long boardId);
+    Long countCommentsWithScope2ForBoard(@Param("boardId")Long boardId);
+
     @Query("SELECT COUNT(c) FROM UserCommentEntity c WHERE c.board.recipeId = :boardId AND c.scope = '1.0'")
-    Long countCommentsWithScope1ForBoard(Long boardId);
+    Long countCommentsWithScope1ForBoard(@Param("boardId")Long boardId);
 
 
 }
