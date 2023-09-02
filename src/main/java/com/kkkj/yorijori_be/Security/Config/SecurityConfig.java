962b45845f47f3f3fa32cdc4f5786fdc1d6aa463
onestar99 <jhb990438@gmail.com>
2023-09-03 01:22:48 +0900

    이미지 전송 csrf 문제 disable 처리

--- src/main/java/com/kkkj/yorijori_be/Security/Config/SecurityConfig.java
+++ src/main/java/com/kkkj/yorijori_be/Security/Config/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 
         http
+                .csrf().disable()
                 .authorizeHttpRequests()
                 .requestMatchers("/**").permitAll()
                 .anyRequest().authenticated()
