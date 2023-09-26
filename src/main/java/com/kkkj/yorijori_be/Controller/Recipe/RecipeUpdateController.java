5e867bc5e812dcb95a7ce9091de6f228f1b6d463
HanByeol <jhb990438@gmail.com>
2023-09-26 15:31:00 +0900

    레시피 업데이트 init

---
+++ src/main/java/com/kkkj/yorijori_be/Controller/Recipe/RecipeUpdateController.java
@@ -0,0 +1,9 @@
+package com.kkkj.yorijori_be.Controller.Recipe;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class RecipeUpdateController {
+}
